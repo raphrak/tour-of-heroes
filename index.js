f787b34b5c1f6ae81778e6fcfdd656be4cc867cf
raphrak <rrakotoarisolo@yoobic.com>
2018-08-29 19:04:42 +0100

    fix(heroku): fixing heroku deployment

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'dist')))
+  .get('/*', (req,res) => { res.sendFile(path.join(__dirname+'/dist/index.html')) })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
