98ef62e10cbc8531e4b85375e46c1c35610a8f6f
raphrak <rrakotoarisolo@yoobic.com>
2018-08-29 18:21:44 +0100

    Initial commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to ng4-animations!');
+    expect(page.getParagraphText()).toEqual('Welcome to tour-of-heroes!');
   });
 });
