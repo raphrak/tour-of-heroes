04b42a54d7d6d96c23d3bbc8f87dd2dc4abe037e
raphrak <rrakotoarisolo@yoobic.com>
2018-08-30 03:11:32 +0100

    feat(security): adding security to heroku deployment

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const express = require('express')
 const path = require('path')
+var enforce = require('express-sslify')
+var compression = require('compression')
+var helmet = require('helmet')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'dist')))
-  .get('/*', (req,res) => { res.sendFile(path.join(__dirname+'/dist/index.html')) })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+let app = express()
+
+if (process.env.FORCE_SSL === 'true') {
+  app.use(enforce.HTTPS({ // eslint-disable-line new-cap
+      trustProtoHeader: true
+  }))
+}
+
+app.use(helmet.contentSecurityPolicy({
+  'directives': {
+      'scriptSrc': [
+          '\'self\'',
+          'blob:',
+          '\'unsafe-eval\''
+      ],
+      'frame-ancestors': ['\'self\'']
+  }
+}))
+app.use(helmet.frameguard({
+  action: 'sameorigin'
+}))
+app.use(compression())
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/*', (req,res) => { res.sendFile(path.join(__dirname+'/dist/index.html')) })
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
